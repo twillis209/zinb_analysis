759449f649f076ff69db6dd498f323d98a1d5d05
Tom Willis <tom.willis@live.com>
2020-11-19 00:36:02 +0000

    Saving conversion script while it works on single files

---
+++ svenssonApproach/Data/output/sheng/convertADtoSCE.py
@@ -0,0 +1,37 @@
+import anndata
+import anndata2ri
+import argparse
+import os
+from rpy2 import robjects
+
+parser = argparse.ArgumentParser(description="Reads AnnotatedDataFrame object from file, converts to R object of the SingleCellExperiment class, and writes the R object to an RData file.")
+parser.add_argument("-if", "--h5adFilePath", type=str, help="Path to h5ad file containing AnnotatedDataFrame")
+parser.add_argument("-of", "--rdataFilePath", type=str, help="Path to RData file to contain converted SingleCellExperiment objects")
+
+args = parser.parse_args()
+
+anndata2ri.activate()
+
+ad = anndata.read(args.h5adFilePath)
+
+name = os.path.splitext(os.path.basename(args.h5adFilePath))[0]
+
+sexp=anndata2ri.py2rpy(ad)
+
+robjects.r.assign(name, sexp)
+
+robjects.r("save({},file=\'test.RData\')".format(name,args.rdataFilePath))
+
+anndata2ri.deactivate()
+
+"""
+import anndata
+import anndata2ri
+import argparse
+import os
+from rpy2 import robjects
+ad=anndata.read('293T_singleCell_exonU.h5ad')
+sexp=anndata2ri.py2rpy(ad)
+robjects.r.assign('my_df', sexp) 
+robjects.r("save(`my_df`,file='test.RData')")
+"""
